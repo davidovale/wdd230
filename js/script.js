853d5db28ede01ed1c44b3a45d226d85015281d0
davidovale <dov21001@byui.edu>
2022-01-13 23:58:59 -0300

    current year added via js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 function teste(){
-    document.getElementById("date").innerHTML = "Last Updated: " + document.lastModified;
+    document.querySelector("#date").innerHTML = "Last Updated: " + document.lastModified;
     document.querySelector("#copy").innerHTML = "&copy;"
   }
   
