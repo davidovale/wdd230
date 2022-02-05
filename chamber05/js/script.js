6824012c0b79967be7c1555094cc0491ce0207c1
davidovale <dov21001@byui.edu>
2022-02-05 07:35:10 -0300

    chamber week 5 added

---
+++ chamber05/js/script.js
@@ -0,0 +1,19 @@
+function date(){
+    const datefieldUK = document.querySelector("#date");
+    const now = new Date();
+    const fulldateUK = new Intl.DateTimeFormat("en-UK", {dateStyle: "full"}).format(now);
+    datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
+    document.querySelector("#date-footer").innerHTML = "Last Modification: " + document.lastModified;
+    document.querySelector("#copy").innerHTML = "&copy;"
+    document.querySelector('#year').innerHTML = new Date(Date.now()).getFullYear();
+}
+
+function toggleMenu(){
+    document.getElementById("primaryNav").classList.toggle("open");
+    document.getElementById("hamburguerBtn").classList.toggle("open");
+}
+
+const x = document.getElementById("hamburguerBtn");
+x.onclick = toggleMenu;
+
+
