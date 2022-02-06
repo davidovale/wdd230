df1f1a3e60ccb5812e29d6e3c5ce70e7c4cbc6c9
davidovale <dov21001@byui.edu>
2022-02-06 03:48:37 -0300

    general updates

--- chamber05/js/script.js
+++ chamber05/js/script.js
@@ -2,7 +2,12 @@ function date(){
     const datefieldUK = document.querySelector("#date");
     const now = new Date();
     const fulldateUK = new Intl.DateTimeFormat("en-UK", {dateStyle: "full"}).format(now);
-    datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
+    if (now.getDay() == 1 || now.getDay() == 2){
+        datefieldUK.innerHTML = '🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.';
+    }else{
+        datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
+    }
+    
     document.querySelector("#date-footer").innerHTML = "Last Modification: " + document.lastModified;
     document.querySelector("#copy").innerHTML = "&copy;"
     document.querySelector('#year').innerHTML = new Date(Date.now()).getFullYear();
