dc0f8f12e6f2b29046df839722132f7ce3ed7484
davidovale <dov21001@byui.edu>
2022-03-06 02:02:21 -0300

    template okay

--- lesson9b/js/script.js
+++ lesson9b/js/script.js
@@ -1,14 +1,22 @@
-const requestURL = 'https://run.mocky.io/v3/d92fd359-0312-46f1-ae6e-187e9211041e';
+//const requestURL = 'https://run.mocky.io/v3/d92fd359-0312-46f1-ae6e-187e9211041e';
+// I usually use https://designer.mocky.io/design, but you said to create the file.
+const requestURL = 'data/data.json';
 const companies = document.querySelector('.companies');
+const companiesList = document.querySelector('.companiesList');
+let choice = "";
 
   function displayCompanies(company) {
     // Create elements to add to the document
     let section = document.createElement('section');
-    let name = document.createElement('h2');
+    let sectionList = document.createElement('section');
+    let name = document.createElement('p');
     let logo = document.createElement('img');
-    let address = document.createElement('h3');
-    let tel = document.createElement('h3');
+    let address = document.createElement('p');
+    let address1 = document.createElement('p');
+    let tel = document.createElement('p');
+    let tel1 = document.createElement('p');
     let site = document.createElement('a');
+    let site1 = document.createElement('a');
 
 
     logo.setAttribute('src',company.imageurl);
@@ -18,29 +26,27 @@ const companies = document.querySelector('.companies');
 
     site.setAttribute('href', company.site);
     site.setAttribute('target', 'blank');
-/*
-    
-    h3Birth.setAttribute('class', "birth");
-    h3Death.setAttribute('class', "death");
-*/  
+
     name.textContent = company.name;
     address.textContent = company.address;
+    address1.textContent = company.address;
     tel.textContent = company.phone;
+    tel1.textContent = company.phone;
     site.textContent = company.site;
+    site1.textContent = company.site;
 
     section.appendChild(logo);
-    section.appendChild(address);
-    section.appendChild(tel);
-    section.appendChild(site);
+    section.appendChild(address1);
+    section.appendChild(tel1);
+    section.appendChild(site1);
 
-    //section.appendChild(name);
+    sectionList.appendChild(name);
+    sectionList.appendChild(address);
+    sectionList.appendChild(tel);
+    sectionList.appendChild(site);
 
-//    card.appendChild(h3Birth);
-//    card.appendChild(h3Death);
-//    card.appendChild(h3Local);
-//    card.appendChild(portrait);
-    
     companies.appendChild(section);
+    companiesList.appendChild(sectionList);
     
   }
 
@@ -54,5 +60,13 @@ const companies = document.querySelector('.companies');
     comp.forEach(displayCompanies);
   });
   
-
+function view(option){
+  if (option == "large"){
+    document.getElementById("companies").style.display="";
+    document.getElementById("companiesList").style.display="none";
+  }else if (option == "small"){
+    document.getElementById("companiesList").style.display="";
+    document.getElementById("companies").style.display="none";
+  }
+}
   
