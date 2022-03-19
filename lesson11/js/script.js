045b994ea9d9919b9f26e9b9a97e6046298728b3
davidovale <dov21001@byui.edu>
2022-03-19 16:11:08 -0300

    div middle-main working fine

--- lesson11/js/script.js
+++ lesson11/js/script.js
@@ -1,7 +1,9 @@
-
-
-
+const requestURL2 = 'data/data.json';
+const companies = document.querySelector('.spotlight');
 const images = document.querySelectorAll("[data-src]");
+let count_spot = 0;
+let auxSpot = 0;
+let numeros = []
 
 function preloadImage(img){
     const src = img.getAttribute("data-src")
@@ -33,6 +35,80 @@ images.forEach(image => {
     imgObserver.observe(image);
 });
 
+function displaySpotlight(company){
+    if (company.membership == "silver" || company.membership == "gold"){
+            let div = document.createElement('div')
+            let h2 = document.createElement('h2')
+            let img = document.createElement('img')
+            let phrase = document.createElement('p')
+            let site = document.createElement('a')
+
+            phrase.textContent = company.phrase
+            site.textContent = company.site
+
+            div.setAttribute('class', "spotlight-1")
+            div.setAttribute('id', "spotlight-"+count_spot)
+
+            img.setAttribute('src',company.imageurl);
+            img.setAttribute('alt', company.name);
+            img.setAttribute('loading', 'lazy');
+
+            site.setAttribute('href', company.site);
+            site.setAttribute('target', 'blank');
+
+            div.appendChild(img);
+            div.appendChild(phrase);
+            div.appendChild(site);
+
+            companies.appendChild(div);
+            count_spot += 1;        
+    }
+    
+
+}
+
+fetch(requestURL2)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    console.table(jsonObject);  
+    const comp = jsonObject['companies'];
+    comp.forEach(displaySpotlight);
+    sort();
+    
+  });
+
+  function sort(){
+    let random = "";
+
+    for (j=0; j<count_spot; j++){
+        document.getElementById('spotlight-'+j).style.display="none";
+    }
+
+        sorteado(count_spot, 3)
+    
+  }
+
+  function sorteado(max,quant) {
+    
+    let auxR = 0;
+    while (numeros.length < quant) {
+        auxR = 0;
+        e = Math.floor(Math.random() * max);
+        for (a=0; a<numeros.length; a++){
+            if(numeros[a] == e){
+                auxR = 1;
+            }
+        }
+      
+      if (auxR == 0) {
+        numeros.push(e)
+      }
+      document.getElementById('spotlight-'+e).style.display="";
+    }
+   
+}
 
   
 function view(option){
