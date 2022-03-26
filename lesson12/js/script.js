354624c7bf3a4f25298d00962f56be738e47fca5
davidovale <dov21001@byui.edu>
2022-03-26 06:04:58 -0300

    temple page okay

--- lesson12/js/script.js
+++ lesson12/js/script.js
@@ -8,19 +8,14 @@ let auxTime = 0;
   function displayTemples(temple) {
     // Create elements to add to the document
     let section = document.createElement('section');
-    let sectionList = document.createElement('section');
     let sectionPlus = document.createElement('div');
     let sectionHistory = document.createElement('div');
-    let name = document.createElement('p');
+    let name = document.createElement('h2');
     let image = document.createElement('img');
     let address = document.createElement('p');
     let address1 = document.createElement('p');
     let tel = document.createElement('p');
-    let tel1 = document.createElement('p');
     let email = document.createElement('a');
-    let email1 = document.createElement('a');
-    let services = document.createElement('p')
-    let services1 = document.createElement('p')
     let history = document.createElement('span');
     let closure = document.createElement('span');
     let lblServices = document.createElement('p');
@@ -31,6 +26,7 @@ let auxTime = 0;
     let lblHistory = document.createElement('p');
     let btnPlus = document.createElement('button');
     let btnLess = document.createElement('button');
+    let btnLike = document.createElement('img');
     const auxSectionPlus = "plus" + temple.id;
     const auxHistory = "history" + temple.id;
     const auxBtnPlus = "btnPlus" + temple.id;
@@ -39,6 +35,7 @@ let auxTime = 0;
     
     image.setAttribute('src',temple.imageurl);
     image.setAttribute('alt', temple.name);
+    image.setAttribute('class', 'temple-images');
     image.setAttribute('loading', 'lazy');
     sectionPlus.setAttribute('class', 'section-plus');
     sectionHistory.setAttribute('class', 'section-history');
@@ -51,7 +48,7 @@ let auxTime = 0;
     lblClosure.setAttribute('class', 'negrito');
     lblHistory.setAttribute('class', 'negrito');
     history.setAttribute('class', 'items');
-    email1.setAttribute('class', "txtEmail")
+    email.setAttribute('class', "txtEmail");
     
     btnPlus.setAttribute('id', auxBtnPlus);
     btnPlus.setAttribute('value', '+');
@@ -66,16 +63,30 @@ let auxTime = 0;
     const auxFunctionLess = 'btnLess("'+temple.id+'")';
     btnLess.setAttribute('onclick', auxFunctionLess);
     btnLess.textContent = "-";
+
+    let auxBtnLike = "like"+temple.id;
+    btnLike.setAttribute('id', auxBtnLike);
+    btnLike.setAttribute('alt', temple.id);
+    btnLike.setAttribute('class', 'btn-like');
+    let valueLike = getLike(temple.id);
+    if(valueLike == "yes"){
+      btnLike.setAttribute('src', 'images/like_yes.png');
+      
+    }else{
+      btnLike.setAttribute('src', 'images/like_no.png');
+    }
+    
+    const setLikeFunction = 'setLike("'+temple.id+'")';
+    btnLike.setAttribute('onclick', setLikeFunction);
+
+    createLikes(temple.id);
     
     //image.setAttribute('onclick', 'newPopup()')
 
     name.textContent = temple.name;
     address.textContent = temple.address;
-    address1.textContent = temple.address;
     tel.textContent = temple.phone;
-    tel1.textContent = temple.phone;
     email.textContent = temple.email;
-    email1.textContent = temple.email;
     closure.textContent = temple.closure;
     history.textContent = temple.history;
     lblServices.textContent = "Services";
@@ -84,8 +95,6 @@ let auxTime = 0;
     lblSealment.textContent = "Sealment";
     lblClosure.textContent = "Closure";
     lblHistory.textContent = "History";
-
-    
 
     sectionPlus.appendChild(lblServices);
 
@@ -127,25 +136,20 @@ let auxTime = 0;
   sectionHistory.appendChild(lblHistory);
   
 
-
+    section.appendChild(name);
     section.appendChild(image);
-    section.appendChild(address1);
-    section.appendChild(tel1);
-    section.appendChild(email1);
+    section.appendChild(address);
+    section.appendChild(tel);
+    section.appendChild(email);
     section.appendChild(btnPlus);
     section.appendChild(btnLess);
+    section.appendChild(btnLike);
     
     section.appendChild(sectionPlus);
     sectionHistory.appendChild(history);
     section.appendChild(sectionHistory);
 
-    sectionList.appendChild(name);
-    sectionList.appendChild(address);
-    sectionList.appendChild(tel);
-    sectionList.appendChild(email);
-
     temples.appendChild(section);
-    templesList.appendChild(sectionList);
     hide(temple.id);
   }
 
@@ -158,17 +162,6 @@ let auxTime = 0;
     const data = jsonObject['temples'];
     data.forEach(displayTemples);
   });
-  
-function view(option){
-  if (option == "large"){
-    document.getElementById("temples").style.display="";
-    document.getElementById("templesList").style.display="none";
-  }else if (option == "small"){
-    document.getElementById("templesList").style.display="";
-    document.getElementById("temples").style.display="none";
-  }
-}
-
 
 
 function date(){
@@ -194,15 +187,6 @@ function toggleMenu(){
 const x = document.getElementById("hamburguerBtn");
 x.onclick = toggleMenu;
 
-function newPopup(){
-    varWindow = window.open (
-    'popupRio.html',
-    'pagina',
-    "width=350, height=255, top=100, left=110, scrollbars=no " );
-    }
-
-
-
 function hide(id){
   document.getElementById("plus"+id).style.display="none";
   document.getElementById("history"+id).style.display="none";
@@ -219,3 +203,75 @@ function btnLess(id){
   document.getElementById("history"+id).style.display="none";
 }
 
+function createLikes(like){
+  const auxLike = "like"+like;
+  //window.localStorage.setItem(auxLike, "no");
+
+}
+
+function setLike(like){
+  const auxLike = "like"+like;
+  const getItemValue = getLike(like);
+  if (getItemValue == "yes"){
+    localStorage.setItem(auxLike, "no");
+  }else{
+    localStorage.setItem(auxLike, "yes");
+  }
+  window.location.reload();
+}
+
+function getLike(like){
+  const auxLike = "like"+like;
+  return window.localStorage.getItem(auxLike);
+}
+// initialize display elements
+//const todayDisplay = document.querySelector(".today");
+const visitsDisplay = document.querySelector(".visits");
+
+// get the stored value in localStorage
+let numVisits = Number(window.localStorage.getItem("visits-ls"));
+let auxLast = "";
+let auxToday = "";
+let lastModified = "";
+
+Modification();
+// determine if this is the first visit or display the number of visits.
+if (numVisits !== 0) {
+	visitsDisplay.textContent = numVisits;
+} else {
+	visitsDisplay.textContent = `This is your first visit!`;
+}
+
+
+// increment the number of visits.
+numVisits++;
+// store the new number of visits value
+localStorage.setItem("visits-ls", numVisits);
+
+
+function currentDate(){
+    const time = new Date()
+    let dia = String(time.getDate()).padStart(2, '0');
+    let mes = String(time.getMonth() + 1).padStart(2, '0');
+    let ano = time.getFullYear();
+    dataAtual = mes + '/' + dia + '/' + ano;
+
+    return dataAtual;
+}
+
+function calculateDate(today, last){
+    let d1 = new Date(today);
+    let d2 = new Date(last);
+    const diffTime = Math.abs(d1 - d2);
+    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)); 
+    //console.log(diffTime + " milliseconds");
+    //console.log(diffDays + " days");
+
+    return `${diffDays} day(s)`;
+}
+
+function Modification(){
+    lastModified = document.lastModified
+    document.querySelector(".lastModification").innerHTML = "Last Update: " + lastModified;
+  }
+  
